5e587f231b865b28d10a102dc8ee10c90001e812
knaye <knaye@allstate.com>
2017-02-06 15:44:25 +0530

    List

--- src/App.js
+++ src/App.js
@@ -13,4 +13,14 @@ class App extends Component {
   }
 }
 
+
+{/*<Box text="sara@gmail.com" id="3" css="selected" click={fn1}  >
+<Box text="chemistry" id="5" css="empty" click={fn2}  >*/}
+
+/*var students = [
+  id:3, text:"bob", css:"selected", click:{fn},
+  id:4, text:"sam", css:"empty", click:{fn}
+]
+<List header="Students" items={students}>*/
+
 export default App;
